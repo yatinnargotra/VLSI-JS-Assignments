6e0849c14051d63d96df96f7f1e95483f57ead0d
Shantanu <shantanuwable2003@gmail.com>
2024-11-08 18:58:52 +0530

    Update script to download entire repo and unzip it

--- cli.sh
+++ cli.sh
@@ -1,41 +1,11 @@
 #!/bin/bash
 
-REPO_OWNER="yatinnargotra"
-REPO_NAME="VLSI-JS-Assignments"
-GITHUB_API="https://api.github.com"
-RAW_CONTENT_URL="https://raw.githubusercontent.com"
+OWNER="yatinnargotra"
+REPO="VLSI-JS-Assignments"
+BRANCH="main"
 
-files_json=$(wget -qO- "${GITHUB_API}/repos/${REPO_OWNER}/${REPO_NAME}/contents/")
+wget "https://github.com/$OWNER/$REPO/archive/refs/heads/$BRANCH.zip" -O "$REPO-$BRANCH.zip"
 
-if echo "$files_json" | grep -q "Not Found"; then
-    echo "Error: Directory not found."
-    exit 1
-fi
+unzip "$REPO-$BRANCH.zip"
 
-# Parse and display files
-echo "Folders in the repository:"
-file_list=$(echo "$files_json" | sed -n 's/.*"name": "\([^"]*\)".*/\1/p')
-counter=1
-while IFS= read -r file; do
-    echo "$counter) $file"
-    counter=$((counter + 1))
-done <<<"$file_list"
-
-echo "Enter the number of the file you want to download:"
-read file_number
-
-selected_file=$(echo "$file_list" | sed -n "${file_number}p")
-
-if [ -z "$selected_file" ]; then
-    echo "Invalid selection."
-    exit 1
-fi
-
-# wget -q "${RAW_CONTENT_URL}/${REPO_OWNER}/${REPO_NAME}/main/${subject}/${selected_file}" -O "$selected_file"
-
-# if [ $? -eq 0 ]; then
-#     echo "File '$selected_file' has been downloaded successfully."
-# else
-#     echo "Error downloading the file."
-#     exit 1
-# fi
+rm "$REPO-$BRANCH.zip"
